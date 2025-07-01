9c949f4ab211a7eff355de03a736d982a58a485b
Hemant Rajput <106817003+hemantHQ@users.noreply.github.com>
2025-07-01 16:08:44 +0530

    New_UI

--- Assets/Scripts/UI_Control.cs
+++ Assets/Scripts/UI_Control.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class UI_Control : MonoBehaviour
 {
+    [SerializeField] GameObject Logo;
+    [SerializeField] GameObject Button_1;
+    [SerializeField] GameObject Button_2;
+    [SerializeField] GameObject Single_Panel;
+    [SerializeField] GameObject Multi_Panel;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,31 @@ public class UI_Control : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Single_Button()
+    {
+        Logo.SetActive(false);
+        Button_1.SetActive(false);
+        Button_2.SetActive(false);
+        Multi_Panel.SetActive(false);
+        Single_Panel.SetActive(true);
+    }
+    public void Multi_Button()
+    {
+        Logo.SetActive(false);
+        Button_1.SetActive(false);
+        Button_2.SetActive(false);
+        Single_Panel.SetActive(false);
+        Multi_Panel.SetActive(true);
+    }
+
+    public void Back_Button()
+    {
+        Single_Panel.SetActive(false);
+        Multi_Panel.SetActive(false);
+        Logo.SetActive(true);
+        Button_1.SetActive(true);
+        Button_2.SetActive(true);
     }
 }
